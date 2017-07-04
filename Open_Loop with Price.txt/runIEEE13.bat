63ba493a532783e1706b2f994770d1c34b1c9f8a
htnnguyen <nguyentrunghieu23121@gmail.com>
2017-07-03 23:36:15 -0500

    Add files via upload

---
+++ Open_Loop with Price.txt/runIEEE13.bat
@@ -0,0 +1,18 @@
+set FNCS_FATAL=yes
+set FNCS_LOG_STDOUT=yes
+set FNCS_LOG_LEVEL=DEBUG2
+set FNCS_TRACE=yes
+
+set FNCS_CONFIG_FILE=
+start /b cmd /c fncs_broker 3 ^>broker.log 2^>^&1
+
+set FNCS_CONFIG_FILE=
+start /b cmd /c fncs_player 2d prices.txt ^>player.log 2^>^&1
+
+set FNCS_CONFIG_FILE=eplus.yaml
+start /b cmd /c python eplus_control.py 172800 60 ^>eplus_control.log 2^>^&1
+
+set FNCS_CONFIG_FILE=
+set FNCS_LOG_LEVEL=DEBUG4
+set FNCS_LOG_STDOUT=yes
+start /b cmd /c gridlabd IEEE13.glm ^>gridlabd.log 2^>^&1 
