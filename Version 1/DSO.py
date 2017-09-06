8b4e61324731b3caae52965cb0b0672e241e18bc
Hieu Nguyen <nguyentrunghieu23121@gmail.com>
2017-09-05 19:50:46 -0500

    Add files via upload

--- Version 1/DSO.py
+++ Version 1/DSO.py
@@ -1,57 +1,49 @@
 #	Copyright (C) 2017 Battelle Memorial Institute
 #import json
 import sys
-import warnings
-#import csv
 import fncs
-
-
-
-k=15
 
 			
 #writting to json file
 def turn_on(i,j):
-    #turn on hvac by reducing the temprature setpoints
-	fncs.publish('house_'+str(i+1)+'_'+str(j+1)+'_status', 1)
+	#turn on hvac by reducing the temprature setpoints
+	fncs.publish('house_'+str(i)+'_'+str(j)+'_status', 1)
 	return
 	
 def restore(i,j):
-	fncs.publish('house_'+str(i+1)+'_'+str(j+1)+'_status', 0)
+	fncs.publish('house_'+str(i)+'_'+str(j)+'_status', 0)
 	return
 
 def turn_off(i,j):
-	fncs.publish('house_'+str(i+1)+'_'+str(j+1)+'_status', -1)
+	fncs.publish('house_'+str(i)+'_'+str(j)+'_status', -1)
 	return
  
 def bus_shed(bus):
 	for j in range(12):
-		turn_off(bus-1,j)
+		turn_off(bus,j+1)
 	return
 	
 def bus_restore(bus):
 	for j in range(12):
-		restore(bus-1,j)
+		restore(bus,j+1)
 	return
 	
 def bus_on(bus):
 	for j in range(12):
-		turn_on(bus-1,j)
+		turn_on(bus,j+1)
 	return
 	
 def load_shed(SW):
 	#Load shed example, we shut down node 10-15
-	if SW==0:
+	if SW==1:
 		for bus in range(10,16):
 			bus_restore(bus)
-	elif SW!=0:
+	elif SW==0:
 		for bus in range(10,16):
 			bus_shed(bus)
 	else:
 		for bus in range(10,16):
 			bus_on(bus)
-
-	
 	return
 	
 # fncs.initialize()
@@ -61,12 +53,8 @@ for i in range(15):
 	for j in range(12):
 		houseID[i][j]='house_'+str(i+1)+'_'+str(j+1)#add controller variable name dynamically
 
-
-
 time_granted = 0 # time variable for checking the retuned time from FNCS
 timeSim= 0
-
-
 
 time_granted=0
 time_granted = 0 # time variable for checking the retuned time from FNCS
@@ -85,16 +73,15 @@ while (time_granted < tf*3600):
 		events = fncs.get_events()
 		for key in events:
 			topic = key.decode()
-			value1 = float(fncs.get_value(key).decode())-1
+			value1 = float(fncs.get_value(key).decode())
 			if topic == 'sw_status':
 				value=value1
-	load_shed(value)
+				load_shed(value)
 	if (time_granted < (timeSim + deltaT)) :
 		time_granted = fncs.time_request(timeSim + deltaT)
 	else:
 		timeSim = timeSim + deltaT
 		time_granted = fncs.time_request(timeSim + deltaT)
-        
 
-fncs.finalize()   
-    
+fncs.finalize()
+
