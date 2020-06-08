63daaba060505518079fe72fb32deac1de2e68fd
Hieu Nguyen <hieu.trung.nguyen@emt.inrs.ca>
2020-06-08 18:35:32 -0400

    Delete clean.py

--- For Windows with less sockets/clean.py
+++
@@ -1,7 +0,0 @@
-import os
-for i in range(15):
-	for j in range(12):
-		#houseID[i][j]=
-		os.remove('house_'+str(i+1)+'_'+str(j+1)+'.csv')
-		os.remove('house_controller'+str(i+1)+'_'+str(j+1)+'.log')
-		os.remove('house_'+str(i+1)+'_'+str(j+1)+'.yaml')
